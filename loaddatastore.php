33932c9331677bde0381942da847d246c5b198d6
Stuart Wong <cgs.wong@gmail.com>
2018-06-26 04:28:14 +0000

    feat: add batch and task data load

--- loaddatastore.php
+++ loaddatastore.php
@@ -16,7 +16,7 @@ use Google\Cloud\Datastore\DatastoreClient;
  */
 function add_product(DatastoreClient $datastore, $sku, $prod)
 {
-    $productKey = $datastore->key('SKU-ORIG', $sku);
+    $productKey = $datastore->key('SKU', $sku);
     $product = $datastore->entity(
         $productKey,
         [
@@ -25,6 +25,39 @@ function add_product(DatastoreClient $datastore, $sku, $prod)
         ]);
     $datastore->upsert($product);
     return $product;
+}
+
+/*
+  Batch load Cloud DataStore Kind from remote URL
+
+  @param $projectId
+  @param $url
+*/
+function batch_load_datastore($projectId, $url) {
+  // Create Datastore client
+  $datastore = new DatastoreClient(['projectId' => $projectId]);
+
+  // Enable `allow_url_fopen` to allow reading file from URL
+  ini_set("allow_url_fopen", 1);
+
+  // Read the products listing and load to Cloud Datastore.
+  // Use batches of 20 for a transaction
+  $json = json_decode(file_get_contents($url), true);
+  $productKeys = array();
+  $products = array();
+  $count = 0;
+  foreach($json as $key => $value) {
+    $productKeys[] = $datastore->key('SKU-BATCH', $key);
+    $products[] = $datastore->entity(
+        $productKey[$count],
+        [
+            'created' => new DateTime(),
+            'name' => $value
+        ]);
+    $count++;
+  
+  $datastore->upsertBatch($products);
+  }
 }
 
 /*
@@ -43,7 +76,6 @@ function load_datastore($projectId, $url) {
   // Read the products listing and load to Cloud Datastore.
   // Use batches of 20 for a transaction
   $json = json_decode(file_get_contents($url), true);
-  $count = 1;
   foreach($json as $key => $value) {
     add_product($datastore, $value["sku"], $value["name"]);
   }
@@ -53,7 +85,7 @@ try
 {
 	$projectId = 'development-206303';
 	$url = 'https://raw.githubusercontent.com/BestBuyAPIs/open-data-set/master/products.json';
-	load_datastore($projectId, $url);
+	batch_load_datastore($projectId, $url);
 	return 200;
 } catch (Exception $err) {
 	echo 'Caught exception: ',  $err->getMessage(), "\n";

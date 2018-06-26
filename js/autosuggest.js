b936be3633c67c1790e934e254aefea5a54f844f
Stuart Wong <cgs.wong@gmail.com>
2018-06-26 22:39:17 +0000

    fix: typeahead attempt 1

--- js/autosuggest.js
+++ js/autosuggest.js
@@ -1,20 +1,22 @@
 // Construct the suggestion engine, Bloodhound
 // Use both prefetch and remote data from GDS
-var products = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  local: 'calldatastore.php'
-});
+$(document).ready(function() {
+  var products = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: 'calldatastore.php'
+  });
 
-// Use a minimum character length, 3, before suggestions start...
-// highlight the suggestion...
-// limit the number of suggestions, 10
-$('#productsuggest .typeahead').typeahead({
-  minLength: 3,
-  highlight: true
-}, {
-  name: 'product-entry',
-  display: 'value',
-  limit: 10,
-  source: products
+  // Use a minimum character length, 3, before suggestions start...
+  // highlight the suggestion...
+  // limit the number of suggestions, 10
+  $('#productsuggest .typeahead').typeahead({
+    minLength: 3,
+    highlight: true
+  }, {
+    name: 'product-entry',
+    display: 'name',
+    limit: 10,
+    source: products
+  });
 });

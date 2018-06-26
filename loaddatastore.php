619e444e2c4feee5dafe7503774ca687f34a2880
Stuart Wong <cgs.wong@gmail.com>
2018-06-26 03:59:07 +0000

    chore: minor comment tweak

--- loaddatastore.php
+++ loaddatastore.php
@@ -1,6 +1,8 @@
 <?php
+# Include the autoloader for libraries installed with composer
+require __DIR__ . '/vendor/autoload.php';
 
-require 'vendor/autoload.php';
+# Import the Google Cloud client library
 use Google\Auth\ApplicationDefaultCredentials;
 use Google\Cloud\Datastore\DatastoreClient;
 
@@ -14,12 +16,12 @@ use Google\Cloud\Datastore\DatastoreClient;
  */
 function add_product(DatastoreClient $datastore, $sku, $prod)
 {
-    $productKey = $datastore->key('SKU', $sku);
+    $productKey = $datastore->key('SKU-ORIG', $sku);
     $product = $datastore->entity(
         $productKey,
         [
             'created' => new DateTime(),
-            'name' => strtolower($prod)
+            'name' => $prod
         ]);
     $datastore->upsert($product);
     return $product;
@@ -43,15 +45,7 @@ function load_datastore($projectId, $url) {
   $json = json_decode(file_get_contents($url), true);
   $count = 1;
   foreach($json as $key => $value) {
-    if ($count == 1) {
-		  $transaction = $datastore->transaction();
-    }
     add_product($datastore, $value["sku"], $value["name"]);
-		if ($count == 20) {
-		  $transaction->commit();
-		  $count = 0;
-    }
-    $count++;
   }
 }
 
@@ -60,6 +54,7 @@ try
 	$projectId = 'development-206303';
 	$url = 'https://raw.githubusercontent.com/BestBuyAPIs/open-data-set/master/products.json';
 	load_datastore($projectId, $url);
+	return 200;
 } catch (Exception $err) {
 	echo 'Caught exception: ',  $err->getMessage(), "\n";
 }

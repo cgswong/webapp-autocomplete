36f798c5607ab481cbdc3d55e41232193d5a359d
Stuart Wong <cgs.wong@gmail.com>
2018-06-28 01:54:18 +0000

    chore: separate js for autosuggest

--- js/autosuggest.js
+++ js/autosuggest.js
@@ -1,29 +1,31 @@
-// Construct the suggestion engine, Bloodhound
-// Use both prefetch and remote data from GDS
+// Construct the suggestion engine, Bloodhound, using remote data from GDS
 $(document).ready(function() {
   var products = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('Matches'),
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace(),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: 'calldatastore.php?searchtext=%QUERY',
-    wilcard: '%QUERY'
+    remote: {
+      url: '/getproduct.php?searchtext=%QUERY',
+      wilcard: '%QUERY'
+    }
   });
 
   products.initialize();
+
   // Use a minimum character length, 3, before suggestions start...
   // highlight the suggestion...
   // limit the number of suggestions, 10
-  $('#productsuggest .typeahead').typeahead({
+  $('#productsuggest').typeahead({
     minLength: 3,
     highlight: true
   }, {
-    name: 'product-name',
+    name: 'productsuggest',
     display: 'productsuggest',
     limit: 10,
-    templates: {
-      suggestion: function(data) {
-        return '<div>' + data.name + '</div>';
-      }
-    },
-    source: products
+    source: function(query, process) {
+      return $.get('/getproduct.php?searchtext=' + query, function(data) {
+        return process(data);
+      });
+    }
+    //source: products
   });
 });
